4b5d4f4b7a596f413d7f80dae8502a116a2fe082
walsh9 <walsh9@gmail.com>
2015-10-22 16:45:14 -0400

    Fix model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   belongs_to :user
-  has_one :best_answer, class_name: "Answer"
+  belongs_to :best_answer, class_name: "Answer"
   has_many :answers
 
   validates :title, :content, :user, presence: true
