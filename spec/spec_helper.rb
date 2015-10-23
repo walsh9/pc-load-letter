99f313f4737ea1392c14a060439bd829bca21998
walsh9 <walsh9@gmail.com>
2015-10-23 11:28:39 -0400

    fix typo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,7 +16,7 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-girequire 'capybara/rspec'
+require 'capybara/rspec'
 require 'simplecov'
 SimpleCov.start 'rails'
 
