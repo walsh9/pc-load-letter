a218c0636089c7d0cd26721142bd8058ba17a703
David Tisdall <david.tisdall@katasan.com>
2015-10-23 15:53:01 -0400

    Remove error handling code

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,4 @@ Rails.application.configure do
 
   # Omniauth setup
   OmniAuth.config.full_host = "https://pc-load-letter.herokuapp.com"
-  OmniAuth.config.on_failure = Proc.new { |env|
-  message_key = env['omniauth.error.type']
-  error_description = Rack::Utils.escape(env['omniauth.error'].error_reason)
-  new_path = "#{env['SCRIPT_NAME']}#{OmniAuth.config.path_prefix}/failure?message=#{message_key}&error_description=#{error_description}"
-  Rack::Response.new(['302 Moved'], 302, 'Location' => new_path).finish
-}
 end
