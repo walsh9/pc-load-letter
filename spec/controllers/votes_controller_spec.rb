cad7726721fd81d0c5c38358427e48376f1f116b
maggiedbaker <maggiedbaker@gmail.com>
2015-10-25 20:05:14 -0400

    fix controller wording

--- spec/controllers/votes_controller_spec.rb
+++ spec/controllers/votes_controller_spec.rb
@@ -16,7 +16,7 @@ describe VotesController do
       expect{post :create, :answer_id => voteable_answer.id, :vote => answer_vote_params }.to change(Vote, :count).by(1)
     end
 
-    it "creates a comment vote with valid params" do
+    it "creates a comment vote" do
       stub_current_user(user)
       expect{post :create, :question_id => commentable_question.id, :comment_id => voteable_comment.id, :vote => question_comment_vote_params }.to change(Vote, :count).by(1)
     end
